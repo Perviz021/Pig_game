84526b654fae0bc3794615332e01ef7417dcc7e2
Parviz <perviznamazli021@gmail.com>
2023-02-16 16:01:24 +0400

    score is changed

--- js/main.js
+++ js/main.js
@@ -61,7 +61,7 @@ btnHold.addEventListener("click", function () {
     document.querySelector(`#score-${activePlayer}`).textContent =
       scores[activePlayer];
     // 2.Check if player's score is >=100
-    if (scores[activePlayer] >= 30) {
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false;
       dice.style.display = "none";
